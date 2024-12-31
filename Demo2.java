1cd65d8c08444ff1cbd18e10b2b175ee62ec497a
SwapnaKundu <swapna.kundu0123@gmail.com>
2024-12-31 14:14:23 +0530

    commit in demo2 from main

--- Demo2.java
+++ Demo2.java
@@ -2,5 +2,7 @@ public class Demo2 {
     public static void main(String[] args) {
         System.out.println("from demo2");
         System.out.println("this is for second commit");
+        System.out.println("lets do merge commit - from main");
+
     }
 }
